e5e1d1bb23e77d3ba3d4c21874af4f44310faa1a
Terrell Page <tpage46@gmail.com>
2019-06-04 16:38:59 -0600

    fixed reset and cleaned methods

--- app/src/main/java/edu/cnm/deepdive/fizzbuzz/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/fizzbuzz/controller/MainActivity.java
@@ -1,10 +1,5 @@
 package edu.cnm.deepdive.fizzbuzz.controller;
 
-import static edu.cnm.deepdive.fizzbuzz.model.Round.Category.BUZZ;
-import static edu.cnm.deepdive.fizzbuzz.model.Round.Category.FIZZ;
-import static edu.cnm.deepdive.fizzbuzz.model.Round.Category.FIZZBUZZ;
-import static edu.cnm.deepdive.fizzbuzz.model.Round.Category.NEITHER;
-
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.content.Intent;
@@ -48,13 +43,17 @@ public class MainActivity extends AppCompatActivity
   private ViewGroup valueContainer;
   private Rect displayRect = new Rect();
   private GestureDetectorCompat detector;
-  private Timer timer;
+  private Timer valueTimer;
+  private Timer gameTimer;
   private SharedPreferences preferences;
   private Game game;
   private int numDigits;
   private int timeLimit;
   private int gameDuration;
-  //private Animator fade;
+  private long gameTimerStart;
+  private long gameTimeElapsed;
+  String gameDataKey;
+  String gameTimeElapsedKey;
 
   /**
    * Initializes this activity when created, and when restored after {@link #onDestroy()} (for
@@ -74,18 +73,19 @@ public class MainActivity extends AppCompatActivity
     preferences = PreferenceManager.getDefaultSharedPreferences(this);
     preferences.registerOnSharedPreferenceChangeListener(this);
     readSettings();
+    String gameDataKey = getString(R.string.game_data_key);
+    String gameTimeElapsedKey = getString(R.string.game_time_elapsed_key);
     if (savedInstanceState != null) {
-      String gameDataKey = getString(R.string.game_data_key);
       game = (Game) savedInstanceState.getSerializable(gameDataKey);
+      gameTimeElapsed = savedInstanceState.getLong(gameTimeElapsedKey, 0);
     }
     if (game == null) {
       game = new Game(timeLimit, numDigits, gameDuration);
     }
-  //  fade = AnimatorInflater.loadAnimator(this, R.animator.indicator_fade);
   }
 
   /**
-   * Updates timer(s) and UI to return display &amp; Game to the pre-{@link #onPause()} state.
+   * Updates valueTimer(s) and UI to return display &amp; Game to the pre-{@link #onPause()} state.
    */
   @Override
   protected void onResume() {
@@ -94,13 +94,13 @@ public class MainActivity extends AppCompatActivity
   }
 
   /**
-   * Captures current state of timer(s) to fields, for possible saving by subsequent {@link
+   * Captures current state of valueTimer(s) to fields, for possible saving by subsequent {@link
    * #onSaveInstanceState(Bundle)} invocation and/or restoration by {@link #onResume()} invocation.
    */
   @Override
   protected void onPause() {
     super.onPause();
-    // TODO Record any in-progress data from timers, etc. to fields.
+    pauseGame();
   }
 
   /**
@@ -111,8 +111,8 @@ public class MainActivity extends AppCompatActivity
   @Override
   protected void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
-    String gameDataKey = getString(R.string.game_data_key);
     outState.putSerializable(gameDataKey, game);
+    outState.putLong(gameTimeElapsedKey, gameTimeElapsed);
   }
 
   /**
@@ -140,10 +140,10 @@ public class MainActivity extends AppCompatActivity
   public boolean onPrepareOptionsMenu(Menu menu) {
     MenuItem play = menu.findItem(R.id.play);
     MenuItem pause = menu.findItem(R.id.pause);
-    play.setEnabled(!running);
-    play.setVisible(!running);
-    pause.setEnabled(running);
-    pause.setVisible(running);
+    play.setEnabled(!running && !complete);
+    play.setVisible(!running && !complete);
+    pause.setEnabled(running && !complete);
+    pause.setVisible(running && !complete);
     return true;
   }
 
@@ -169,8 +169,9 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
         break;
       case R.id.replay:
-        pauseGame();
         game = new Game(timeLimit, numDigits, gameDuration);
+        gameTimeElapsed = 0;
+        complete = false;
         pauseGame();
         break;
       case R.id.status:
@@ -233,7 +234,9 @@ public class MainActivity extends AppCompatActivity
 
   private void pauseGame() {
     running = false;
-    stopTimer();
+    stopValueTimer();
+    stopGameTimer();
+    valueDisplay.setText("");
     // TODO Update any additional necessary fields.
     invalidateOptionsMenu();
   }
@@ -242,21 +245,32 @@ public class MainActivity extends AppCompatActivity
     running = true;
     if (game == null) {
       game = new Game(timeLimit, numDigits, gameDuration);
+      gameTimeElapsed = 0;
     }
     updateValue();
+    startGameTimer();
+    startValueTimer();
     // TODO Update any additional necessary fields.
     invalidateOptionsMenu();
   }
 
-  private void stopTimer() {
-    if (timer != null) {
-      timer.cancel();
-      timer = null;
+  private void stopValueTimer() {
+    if (valueTimer != null) {
+      valueTimer.cancel();
+      valueTimer = null;
     }
   }
 
-  private void recordRound(Round.Category selection) {
-    Round.Category category = Round.Category.fromValue(value);
+  private void stopGameTimer() {
+    if (gameTimer != null) {
+      gameTimer.cancel();
+      gameTimer = null;
+      gameTimeElapsed += System.currentTimeMillis() - gameTimerStart;
+    }
+  }
+
+  private void recordRound(Category selection) {
+    Category category = Category.fromValue(value);
     Round round = new Round(value, category, selection);
     game.add(round);
     ImageView indicator;
@@ -272,7 +286,8 @@ public class MainActivity extends AppCompatActivity
         break;
       case FIZZBUZZ:
         indicator = findViewById(
-            round.isCorrect() ? R.id.correct_fizzbuzz_indicator : R.id.incorrect_fizzbuzz_indicator);
+            round.isCorrect() ? R.id.correct_fizzbuzz_indicator
+                : R.id.incorrect_fizzbuzz_indicator);
         break;
       default:
         indicator = findViewById(
@@ -284,11 +299,7 @@ public class MainActivity extends AppCompatActivity
   }
 
   private void updateValue() {
-    int numDigits = preferences.getInt(getString(R.string.num_digits_key),
-        getResources().getInteger(R.integer.num_digits_default));
     int valueLimit = (int) Math.pow(10, numDigits) - 1;
-    int timeLimit = preferences.getInt(getString(R.string.time_limit_key),
-        getResources().getInteger(R.integer.time_limit_default));
     int containerHeight = valueContainer.getHeight();
     int containerWidth = valueContainer.getWidth();
     int textHeight;
@@ -306,10 +317,19 @@ public class MainActivity extends AppCompatActivity
     displayRect.bottom = (containerHeight + textHeight) / 2;
     displayRect.left = (containerWidth - textWidth) / 2;
     displayRect.right = (containerWidth + textWidth) / 2;
+  }
+
+  private void startValueTimer() {
     if (timeLimit != 0) {
-      timer = new Timer();
-      timer.schedule(new TimeoutTask(), timeLimit * 1000);
+      valueTimer = new Timer();
+      valueTimer.schedule(new TimeoutTask(), timeLimit * 1000);
     }
+  }
+
+  private void startGameTimer() {
+    gameTimer = new Timer();
+    gameTimer.schedule(new GameTimeoutTask(), 1000L * gameDuration - gameTimeElapsed);
+    gameTimerStart = System.currentTimeMillis();
   }
 
   private class TimeoutTask extends TimerTask {
@@ -319,19 +339,17 @@ public class MainActivity extends AppCompatActivity
       runOnUiThread(() -> {
         recordRound(null);
         updateValue();
+        startValueTimer();
       });
     }
   }
 
   private class GameTimeoutTask extends TimerTask {
 
-    /**
-     * The action to be performed by this timer task.
-     */
     @Override
     public void run() {
       complete = true;
-      pauseGame();
+      runOnUiThread(() -> pauseGame());
     }
   }
 
@@ -363,7 +381,7 @@ public class MainActivity extends AppCompatActivity
           deltaX * deltaX / radiusX / radiusX + deltaY * deltaY / radiusY / radiusY;
       double speed = Math.hypot(velocityX, velocityY);
       if (speed >= SPEED_THRESHOLD && ellipticalDistance >= 1) {
-        stopTimer();
+        stopValueTimer();
         Category selection;
         if (Math.abs(deltaY) * containerWidth <= Math.abs(deltaX) * containerHeight) {
           if (deltaX > 0) {
@@ -380,6 +398,7 @@ public class MainActivity extends AppCompatActivity
         }
         recordRound(selection);
         updateValue();
+        startValueTimer();
         handled = true;
       }
       return handled;
--- app/src/main/java/edu/cnm/deepdive/fizzbuzz/controller/StatusActivity.java
+++ app/src/main/java/edu/cnm/deepdive/fizzbuzz/controller/StatusActivity.java
@@ -13,12 +13,14 @@ import java.util.List;
 
 public class StatusActivity extends AppCompatActivity {
 
+  private ListView roundList;
+
   @SuppressLint("SetTextI18n")
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_status);
-    ListView roundList = findViewById(R.id.round_list);
+    roundList = findViewById(R.id.round_list);
     Game game = (Game) getIntent().getSerializableExtra(getString(R.string.game_data_key));
     List<Round> rounds = game.getRounds();
     int totalRounds = rounds.size();
